e413f684f076b84268c419a7f7c380ea1a76f8a4
charles peters <charlespeters42@gmail.com>
2016-12-06 11:57:23 -0800

    representing colors and families as object

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 // Uses webpack to load in the JSON
 import obsidian from 'json!obsidian.css/css.json'
-import families from 'json!ff.css/families.json'
 
 import cnf from './lib/config'
 import spacing from './lib/spacing'
 import typescale from './lib/typescale'
+import color from './lib/color'
+import families from './lib/families'
 
-function KNDLG (config = cnf) {
+function kndlng (config = cnf) {
   return {
-    Obsidian: obsidian,
-    Families: families,
-    Spacing: spacing,
-    Typescale: typescale(config.spacing.baseSp, config.spacing.unit)
+    obsidian,
+    families,
+    spacing,
+    color,
+    typescale: typescale(config.spacing.baseSp, config.spacing.unit)
   }
 }
 
@@ -19,7 +21,8 @@ export {
   typescale as Typescale,
   families as Families,
   obsidian as Obsidian,
+  color as Color,
   spacing as Spacing
 }
 
-export default KNDLG
+export default kndlng
